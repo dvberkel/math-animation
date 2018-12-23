fd14382d91febebb707429ceb42b2c38a4ebeae5
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2018-12-23 08:14:36 +0100

    Draw the traveling point

--- js/sketch.js
+++ js/sketch.js
@@ -4,16 +4,20 @@ function setup(){
 }
 
 function draw(){
+    background(color(0, 0, 100));
     const x = width/2;
     const y = height/2;
     const radius = 200;
     stroke(0, 0, 0);
+    noFill();
     ellipse(x, y, 2*radius, 2*radius);
     const N = 10;
     for(var index=3; index < N; index++) {
         var color_value = map(index, 3, N, 0, 360);
         stroke(color_value, 100, 100);
-        regular_polygon(index, 320, 240, 200);
+        regular_polygon(index, x, y, radius);
+        fill(color_value, 100 , 100);
+        travel(millis(), index, x, y, radius);
     }
 
 }
@@ -29,4 +33,16 @@ function regular_polygon(n, x, y, r) {
 function regular_vertex(index, n, x, y, r) {
     const angle = TWO_PI/n;
     return [r*cos(index*angle) + x, r*sin(index*angle) + y];
+}
+
+function travel(t, n, x, y, r) {
+    const nt = t/1000;
+    const fnt = floor(nt);
+    const delta = nt - fnt;
+    let u = regular_vertex(fnt, n, x, y, r);
+    let v = regular_vertex(fnt + 1, n, x, y, r);
+    let cx = (1-delta)*u[0] + delta*v[0];
+    let cy = (1-delta)*u[1] + delta*v[1];
+    const cr = 10;
+    ellipse(cx, cy, cr, cr);
 }
