25efb0f85e831551ca331ec11ca0fb27b98b9d3d
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2018-12-23 07:38:41 +0100

    Draw regular polygons

--- js/sketch.js
+++ js/sketch.js
@@ -3,10 +3,21 @@ function setup(){
 }
 
 function draw(){
-    if (mouseIsPressed) {
-        fill(0);
-    } else {
-        fill(255);
+    stroke(0);
+    for(var index=3; index < 10; index++) {
+        regular_polygon(index, 320, 240, 200);
     }
-    ellipse(mouseX, mouseY, 80, 80);
+}
+
+function regular_polygon(n, x, y, r) {
+    for(var index=0; index < n; index++) {
+        let u = regular_vertex(index, n, x, y, r);
+        let v = regular_vertex(index + 1, n, x, y, r);
+        line(u[0], u[1], v[0], v[1]);
+    }
+}
+
+function regular_vertex(index, n, x, y, r) {
+    const angle = TWO_PI/n;
+    return [r*cos(index*angle) + x, r*sin(index*angle) + y];
 }
